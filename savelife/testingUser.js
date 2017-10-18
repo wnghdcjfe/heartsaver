d0d5c77833efd59ba3a25323bd9278dfdc6f730a
wnghdcjf <jhc9639@naver.com>
2017-10-19 08:46:48 +0900

    plus id to testingUser.js

--- savelife/testingUser.js
+++ savelife/testingUser.js
@@ -1,52 +1,64 @@
 module.exports = ()=>{
   const testingUser = [{
+    "id":0,
     "name": "주홍철",
     "age": 25,
     "disease": "천식"
   }, {
+    "id":1,
     "name": "문선균",
     "age": 25,
     "disease": "정상"
   }, {
+    "id":2,
     "name": "박병윤",
     "age": 22,
     "disease": "정상"
   }, {
+    "id":3,
     "name": "송선빈",
     "age": 22,
     "disease": "정상"
   }, {
+    "id":4,
     "name": "유명환",
-    "age": 40,
+    "age": 30,
     "disease": "흡연"
   }, {
+    "id":5,
     "name": "원빈",
     "age": 30,
     "disease": "정상"
   }, {
+    "id":6,
     "name": "류성룡",
     "age": 31,
     "disease": "정상"
   }, {
+    "id":7,
     "name": "이순신",
     "age": 60,
     "disease": "정상"
   }, {
+    "id":8,
     "name": "최범석",
     "age": 22,
     "disease": "정상"
   }, {
+    "id":9,
     "name": "정지원",
     "age": 22,
     "disease": "정상"
   }, {
+    "id":10,
     "name": "미스터김",
     "age": 50,
     "disease": "정상"
   }, {
+    "id":11,
     "name": "양영주",
     "age": 25,
     "disease": "정상"
   }];
     return testingUser;
-}
+} 
